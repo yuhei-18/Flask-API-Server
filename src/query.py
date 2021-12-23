daf16a3da28cae224d8d250e74c06cf4acf5f4c6
yuhei-18 <kasaiyuhei8@gmail.com>
2021-12-23 23:04:51 +0900

    feat: 各 Query の作成

--- src/query.py
+++ src/query.py
@@ -1,9 +1,11 @@
 import graphene
 from graphene.relay import Node
 from graphene_mongo import MongoengineConnectionField
-from .objects.index import User
+from .objects.index import User, Role, Post
 
 
 class Query(graphene.ObjectType):
     node = Node.Field()
-    all_users = MongoengineConnectionField(User)
+    users = MongoengineConnectionField(User)
+    roles = MongoengineConnectionField(Role)
+    posts = MongoengineConnectionField(Post)
